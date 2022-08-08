6be4b8b424e1ddc9d8c43d0975f7643555496483
Wil31 <w.gavigniaux@gmail.com>
2022-08-08 18:21:44 +0200

    Add user creation form; add user_type to user model

--- users/models.py
+++ users/models.py
@@ -6,9 +6,15 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    class UserType(models.TextChoices):
+        SALES = "SLS"
+        SUPPORT = "SPP"
+        MANAGEMENT = "MNG"
+
     email = models.EmailField("email address", unique=True)
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    user_type = models.fields.CharField(choices=UserType.choices, max_length=3)
     date_created = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
