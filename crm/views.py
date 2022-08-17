26fddd75a440bd3f7f92531379386cb73e3a5f44
Wil31 <w.gavigniaux@gmail.com>
2022-08-17 11:35:52 +0200

    Add destroy methods to views

--- crm/views.py
+++ crm/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Client, Contract, Event
 from .serializers import ClientSerializer, ContractSerializer, EventSerializer
@@ -14,6 +16,14 @@ class ClientViewset(ModelViewSet):
         current_user = self.request.user
         return Client.objects.filter(sales_contact=current_user)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": f"Client '{instance}' deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
+
 
 class ContractViewset(ModelViewSet):
 
@@ -25,6 +35,14 @@ class ContractViewset(ModelViewSet):
         clients = Client.objects.filter(sales_contact=current_user)
         return Contract.objects.filter(client__in=clients)
 
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": f"Contract '{instance}' deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
+
 
 class EventViewset(ReadOnlyModelViewSet):
 
@@ -33,3 +51,11 @@ class EventViewset(ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return Event.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(
+            {"message": f"Event '{instance}' deleted successfully."},
+            status=status.HTTP_200_OK,
+        )
